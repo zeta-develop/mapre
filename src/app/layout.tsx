dc3a9f752b2cbe6a2a94968cce146a5411a94323
Ronald Tellez <80638821+zeta-develop@users.noreply.github.com>
2025-01-26 01:57:08 +0000

    none

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import { ThemeProvider } from '../components/theme-provider'
 import './globals.css'
 import { Inter } from 'next/font/google'
-import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ['latin'] })
 
